95dc0b75371a70447b080a44e8bbbedd35e5c240
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-08-12 09:05:28 +0800

    first commit

---
+++ src/main/java/assessment/core/vttpssf/VttpSsfApplication.java
@@ -0,0 +1,13 @@
+package assessment.core.vttpssf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VttpSsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VttpSsfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/assessment/core/vttpssf/VttpSsfApplicationTests.java
@@ -0,0 +1,13 @@
+package assessment.core.vttpssf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VttpSsfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
